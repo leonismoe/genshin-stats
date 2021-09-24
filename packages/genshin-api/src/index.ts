9200d1177cd9d00c9a0aa639abc5fdad7e473912
Leonismoe <leonismoe@users.noreply.github.com>
2021-09-25 05:00:48 +0800

    chore(genshin-api): bump version to `0.1.2`

---
+++ packages/genshin-api/src/index.ts
@@ -0,0 +1,2 @@
+export { Element, ExplorationType, ReliquaryPosition, SpiralAbyssScheduleType, WeaponType } from './constants';
+export { getGenshinGameStats, getPlayerCharacterDetails, getSpiralAbyssData } from './game-record';
