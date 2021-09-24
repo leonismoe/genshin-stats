f988b126a34fe2f3807145c6c1d3cc49f38f962c
Leonismoe <leonismoe@users.noreply.github.com>
2021-09-25 04:39:05 +0800

    bugfix(api): no longer export `const enum` and bump version to `0.2.1`

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,16 +1,16 @@
-export const enum APIClientType {
+export enum APIClientType {
   IOS = 1,
   ANDROID = 2,
   WEB = 4,     // pc
   WEBVIEW = 5, // webview in android / ios app
 }
 
-export const enum DSServer {
+export enum DSServer {
   CN = 'cn',
   OS = 'os',
 }
 
-export const enum GameBiz {
+export enum GameBiz {
   HONKAI_3RD = 'bh3_cn',
   GENSHIN_IMPACT = 'hk4e_cn',
 }
