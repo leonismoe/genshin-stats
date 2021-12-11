43ee3165f85ad2904eca87ab46b904e39cabccdf
Leonismoe <leonismoe@users.noreply.github.com>
2021-12-11 18:49:12 +0800

    chore(api): add salt for HoYoLab CN v2.18.0 and bump version to 0.2.7

---
+++ packages/api/scripts/loader.mjs
@@ -0,0 +1,26 @@
+import { readFile } from 'node:fs/promises';
+
+/**
+ * @see https://nodejs.org/api/esm.html#resolvespecifier-context-defaultresolve
+ * @param {string} url
+ * @param {{ format: string }} context
+ * @param {Function} defaultLoad
+ * @returns {Promise<{ format: string, source: string | ArrayBuffer | SharedArrayBuffer | Uint8Array }>}
+ */
+export async function load(url, context, defaultLoad) {
+  if (!context.format && url.endsWith('.json')) {
+    if (url.startsWith('file:///')) {
+      url = (new URL(url)).pathname.slice(1);
+    }
+    else if (url.includes('://')) {
+      throw new Error('Loading remote JSON file is prohibited.');
+    }
+
+    return readFile(url, 'utf8').then(text => ({
+      format: 'json',
+      source: text,
+    }));
+  }
+
+  return defaultLoad(url, context, defaultLoad);
+}
