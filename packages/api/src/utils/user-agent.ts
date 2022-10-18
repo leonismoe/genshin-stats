055517c9acaf3f1a8097aa4563c374e22e2369aa
Leonismoe <leonismoe@users.noreply.github.com>
2022-10-19 03:20:05 +0800

    chore(api): update user agents

--- packages/api/src/utils/user-agent.ts
+++ packages/api/src/utils/user-agent.ts
@@ -1,23 +1,23 @@
 // https://stackoverflow.com/a/5916330
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/ios
-export const USER_AGENT_IOS_SAFARI = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1';
-export const USER_AGENT_IOS_CHROME = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/104.0.5112.88 Mobile/15E148 Safari/604.1';
-export const USER_AGENT_IOS_FIREFOX = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/103.0 Mobile/15E148 Safari/605.1.15';
-export const USER_AGENT_IOS_WEBVIEW = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile';
+export const USER_AGENT_IOS_SAFARI = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1';
+export const USER_AGENT_IOS_CHROME = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/106.0.5249.92 Mobile/15E148 Safari/604.1';
+export const USER_AGENT_IOS_FIREFOX = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/106.0 Mobile/15E148 Safari/605.1.15';
+export const USER_AGENT_IOS_WEBVIEW = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile';
 
 // https://developer.chrome.com/docs/multidevice/user-agent/
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/android
-export const USER_AGENT_ANDROID_CHROME = 'Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.69 Mobile Safari/537.36';
-export const USER_AGENT_ANDROID_FIREFOX = 'Mozilla/5.0 (Android 12; Mobile; rv:68.0) Gecko/68.0 Firefox/103.0';
-export const USER_AGENT_ANDROID_WEBVIEW = 'Mozilla/5.0 (Linux; Android 12; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.69 Mobile Safari/537.36';
+export const USER_AGENT_ANDROID_CHROME = 'Mozilla/5.0 (Linux; Android 13; SM-G9900) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.126 Mobile Safari/537.36';
+export const USER_AGENT_ANDROID_FIREFOX = 'Mozilla/5.0 (Android 13; Mobile; SM-G9900; rv:105.0) Gecko/105.0 Firefox/105.0';
+export const USER_AGENT_ANDROID_WEBVIEW = 'Mozilla/5.0 (Linux; Android 13; SM-G9900; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.126 Mobile Safari/537.36';
 
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/windows
-export const USER_AGENT_WINDOWS_CRHOME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36';
-export const USER_AGENT_WINDOWS_EDGE = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.54';
-export const USER_AGENT_WINDOWS_FIREFOX = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0';
+export const USER_AGENT_WINDOWS_CRHOME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
+export const USER_AGENT_WINDOWS_EDGE = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/105.0.1343.42';
+export const USER_AGENT_WINDOWS_FIREFOX = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0';
 
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/macos
-export const USER_AGENT_MACOS_SAFARI = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15';
-export const USER_AGENT_MACOS_CHROME = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36';
-export const USER_AGENT_MACOS_EDGE = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.54';
-export const USER_AGENT_MACOS_FIREFOX = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12.5; rv:103.0) Gecko/20100101 Firefox/103.0';
+export const USER_AGENT_MACOS_SAFARI = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15';
+export const USER_AGENT_MACOS_CHROME = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
+export const USER_AGENT_MACOS_EDGE = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.34';
+export const USER_AGENT_MACOS_FIREFOX = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12.6; rv:106.0) Gecko/20100101 Firefox/106.0';
