9e037e748cb90290317751ad59a8326c1a31fd22
Leonismoe <leonismoe@users.noreply.github.com>
2023-01-16 14:43:53 +0800

    chore(api): add salt for HoYoLab CN v2.43.1

--- packages/api/data/salts.json.d.ts
+++ packages/api/data/salts.json.d.ts
@@ -1,3 +1,10 @@
+// DS1: APIClientType.IOS              : 1: -
+// DS1: APIClientType.ANDROID          : 2: k2
+// DS1: APIClientType.WEB (PC)         : 4: -
+// DS1: APIClientType.WEBVIEW (Mobile) : 5: lk2
+// DS2: APIClientType.ANDROID          : 2: x6
+// DS2: APIClientType.WEBVIEW (Mobile) : 5: x4
+
 import { APIClientType, ValuesOfEnum } from '../typings';
 
 interface Salts {
