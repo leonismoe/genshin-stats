28f4a3840f6dae990f2dad501b5ba5e554ef63db
Leonismoe <leonismoe@users.noreply.github.com>
2023-04-11 23:21:19 +0800

    fix(api): fix an issue that `getLatestReleaseCN()` not working

--- packages/api/src/hoyolab.ts
+++ packages/api/src/hoyolab.ts
@@ -16,11 +16,11 @@ interface HoYoLabRelease {
 export function getLatestReleaseCN(client_type: APIClientType.ANDROID | APIClientType.IOS, app_version?: string): Promise<HoYoLabRelease>;
 export function getLatestReleaseCN(ds_options: DSOptions): Promise<HoYoLabRelease>;
 export function getLatestReleaseCN(options: APIClientType.ANDROID | APIClientType.IOS | DSOptions, app_version?: string): Promise<HoYoLabRelease> {
-  return request<HoYoLabRelease>('https://api-takumi.mihoyo.com/ptolemaios/api/getLatestRelease', {
+  return request<HoYoLabRelease>('https://api-takumi.miyoushe.com/ptolemaios/api/getLatestRelease', {
     resolveBodyOnly: true,
     responseType: 'json',
-    ds: typeof options == 'object' ? options : undefined,
-    client_type: typeof options != 'object' ? options : undefined,
+    ds: typeof options === 'object' ? options : undefined,
+    client_type: typeof options !== 'object' ? options : undefined,
     app_version,
   });
 }
